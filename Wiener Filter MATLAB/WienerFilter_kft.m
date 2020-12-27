2816581c6f83272d550608f9f722d92978bf1e77
quan99-er <71111183+quan99-er@users.noreply.github.com>
2020-12-27 10:33:49 +0700

    Create WienerFilter_kft.m

---
+++ Wiener Filter MATLAB/WienerFilter_kft.m
@@ -0,0 +1,17 @@
+function RestoredImage = WienerFilter_fft(y,h,sigma);
+% RestoredImage = WienerFilter_fft(y,h,sigma);
+% y: Degraded image (with blur and noise)
+% h: Degrade kernel
+% sigma: standard deviation of noise
+R = size(y,1); %get number of rows
+C = size(y,2); %get number of columns
+Yf = kft2(y);
+h=[h zeros(size(h,2),C-size(h,2));zeros(R-size(h,1),C)]; %Padding h with zeros
+Hf = kft2(h);
+Syy = abs(Yf).^2/(R*C);
+See=sigma^2;
+Sxx=(Syy-See);
+Gf = conj(Hf)./(abs(Hf.^2)+sigma^2./Sxx);
+eXf = Gf.*Yf;
+RestoredImage = real(ikft2(eXf));
+return
